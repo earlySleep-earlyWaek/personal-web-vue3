29431f45a3286421760fc837c40a6c26e1b982b7
ESEW <2124683945@qq.com>
2025-12-14 23:59:03 +0800

    账号信息卡

---
+++ src/views/chartRoom/components/ChartUserCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-10px">
+    <div class="flex gap-10px">
+      <div class="w-50px">
+        <el-avatar :size="50" src="/images/logo.png" />
+      </div>
+      <div class="flex-1">
+        <div class="h-1/3">昵称</div>
+        <div class="h-1/3">账号 1231231234567</div>
+        <div class="h-1/3">在线</div>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="w-60px c-#888">签名</div>
+      <div>个人签名</div>
+    </div>
+  </div>
+</template>
