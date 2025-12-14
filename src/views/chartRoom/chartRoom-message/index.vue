4bfae4938ee5f0dfe9a9f2b88eafbae973e7393d
ESEW <2124683945@qq.com>
2025-12-14 17:52:25 +0800

    新增 聊天页面基础ui

--- src/views/chartRoom/chartRoom-message/index.vue
+++ src/views/chartRoom/chartRoom-message/index.vue
@@ -1,25 +1,84 @@
 <template>
   <el-scrollbar class="bg-dark" height="100%">
-    <div class="w-350px h-full">
-      <div class="messageRoom" v-for="item in ChartRoomMessage.messageRoomList">
+    <div class="w-350px h-full flex flex-wrap">
+      <div
+        class="messageRoom"
+        v-for="(item, index) in ChartRoomMessage.messageRoomList"
+        @click="config.changeActivited(index)"
+        :style="config.activitedStyle(index)"
+      >
         <el-avatar :size="50" :src="item.avatar" />
         <div class="flex-1 h-full">
           <div class="h-50% messageRoomTitle">
             {{ item.name }}
           </div>
-          <div class="h-50% messageRoomText">111</div>
+          <div class="h-50% messageRoomText">
+            {{ item.message }}
+          </div>
         </div>
       </div>
     </div>
   </el-scrollbar>
-  <div class="flex-1 h-full bg-coolgray"></div>
+  <div class="flex-1 flex-wrap">
+    <el-scrollbar class="w-full h-[calc(100%-300px)] bg-#303030" height="100%">
+      <div class="messageScroll">
+        <div
+          v-for="(item, index) in ChartRoomMessage.roomMessages"
+          class="w-full h-fit flex gap-10px"
+        >
+          <div v-if="item.name != ChartRoomMessage.userInfo.name" class="flex gap-10px">
+            <el-avatar class="mt-10px" :src="item.avatar" />
+            <div>
+              <div class="messageName">{{ item.name }}</div>
+              <div class="messageBubble">
+                {{ item.message }}
+              </div>
+            </div>
+          </div>
+
+          <div
+            v-if="item.name == ChartRoomMessage.userInfo.name"
+            class="w-full flex gap-10px justify-end"
+          >
+            <div>
+              <div class="messageName">{{ item.name }}</div>
+              <div class="messageBubble">
+                {{ item.message }}
+              </div>
+            </div>
+            <el-avatar class="mt-10px" :src="item.avatar" />
+          </div>
+        </div>
+      </div>
+    </el-scrollbar>
+
+    <div class="inputArea">
+      <div class="inputToolBar">
+        <el-icon class="inputToolBarItem"><Picture /></el-icon>
+        <el-icon class="inputToolBarItem"><Folder /></el-icon>
+      </div>
+      <div class="flex-1"></div>
+      <div class="inputButton">
+        <el-button size="small" type="primary"> 发送</el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
 import { ChartRoomMessage } from '../config'
+import { Folder, Picture } from '@element-plus/icons-vue'
 
-const config = reactive({})
+const config = reactive({
+  activited: null,
+  changeActivited(index) {
+    this.activited = index
+  },
+  activitedStyle(index) {
+    return this.activited == index ? 'background:#ffffff30;' : ''
+  },
+})
 </script>
 
 <style lang="scss" scoped>
@@ -39,7 +98,7 @@ const config = reactive({})
 
   transition: 0.2s;
   &:hover {
-    background-color: #ffffff50;
+    background-color: #ffffff30;
   }
 }
 .messageRoomTitle {
@@ -49,5 +108,80 @@ const config = reactive({})
 }
 .messageRoomText {
   color: #888;
+}
+.messageScroll {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+
+  flex: 1 1 0%;
+  height: 100%;
+
+  box-sizing: border-box;
+  padding: 20px;
+}
+
+.message {
+  display: flex;
+  flex-direction: column;
+
+  justify-content: end;
+}
+
+.messageName {
+  color: #888;
+}
+
+.messageBubble {
+  box-sizing: border-box;
+  padding: 8px;
+  background-color: #ccc;
+
+  border-radius: 8px;
+  margin-top: 5px;
+
+  height: fit-content;
+}
+
+.inputArea {
+  //w-full h-300px
+  width: 100%;
+  height: 300px;
+  background-color: #303030;
+
+  border-top: 1px solid #88888850;
+
+  display: flex;
+  flex-direction: column;
+}
+.inputToolBar {
+  display: flex;
+  gap: 10px;
+
+  box-sizing: border-box;
+  padding: 5px;
+
+  .inputToolBarItem {
+    cursor: pointer;
+
+    color: #fff;
+    font-size: 22px;
+
+    transition: 0.2s;
+    &:hover {
+      color: #2bbbbb;
+    }
+  }
+}
+
+.inputButton {
+  // w-full flex justify-end
+  width: 100%;
+
+  display: flex;
+  justify-content: end;
+
+  box-sizing: border-box;
+  padding: 10px;
 }
 </style>
--- src/views/chartRoom/config.js
+++ src/views/chartRoom/config.js
@@ -1,31 +1,61 @@
 const image = '/images/logo.png'
 
 export const ChartRoomMessage = {
+  userInfo: {
+    name: 'wang',
+  },
   messageRoomList: [
     {
       name: 'gamers',
       avatar: image,
+      message: '又到了上海',
       id: 1,
     },
     {
       name: '相亲相爱一家人',
       avatar: image,
+      message: '下来吃饭',
       id: 2,
     },
     {
-      name: '小美',
+      name: '111111',
       avatar: image,
+      message: '🥰',
       id: 3,
     },
     {
       name: '壮壮',
       avatar: image,
+      message: '在吗',
       id: 4,
     },
     {
       name: '牛爷爷',
       avatar: image,
+      message: '图图~',
       id: 5,
+    },
+  ],
+  roomMessages: [
+    {
+      avatar: image,
+      name: 'wang',
+      message: '你好👋',
+    },
+    {
+      avatar: image,
+      name: '111111',
+      message: '收到👌',
+    },
+    {
+      avatar: image,
+      name: '111111',
+      message: '?',
+    },
+    {
+      avatar: image,
+      name: 'wang',
+      message: '🥰',
     },
   ],
 }
