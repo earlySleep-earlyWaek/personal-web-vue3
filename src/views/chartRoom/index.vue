976af34f0b9181d5db751beb0454b499772d2ef8
ESEW <2124683945@qq.com>
2025-12-14 23:59:46 +0800

    重写登录页

--- src/views/chartRoom/index.vue
+++ src/views/chartRoom/index.vue
@@ -1,53 +1,44 @@
 <template>
   <div class="main">
-    <div class="leftArea">
-      <div class="text-center font-600 font-size-18px cursor-default c-#fff">聊</div>
-      <div class="leftAreaAvatar">
-        <el-avatar :size="30" src="/images/logo.png" />
-      </div>
-      <div
-        class="leftAreaItem"
-        :style="config.activedItemStyle('message')"
-        @click="config.handleActivitedClick('message')"
-      >
-        <el-icon><ChatDotRound /></el-icon>
-      </div>
-      <div
-        class="leftAreaItem"
-        :style="config.activedItemStyle('user')"
-        @click="config.handleActivitedClick('user')"
-      >
-        <el-icon><User /></el-icon>
-      </div>
-      <div
-        class="leftAreaItem"
-        :style="config.activedItemStyle('setting')"
-        @click="config.handleActivitedClick('setting')"
-      >
-        <el-icon><Setting /></el-icon>
-      </div>
+    <div class="w-80% h-100%">
+      <el-carousel :interval="5000" pause-on-hover class="h-100%" height="99%">
+        <el-carousel-item v-for="(item, index) in ChartIndex.carousel" :key="index">
+          <div class="h-10% c-#fff">{{ item.text }}</div>
+          <el-image class="h-90% !rounded-0px" fit="cover" :src="item.image" />
+        </el-carousel-item>
+      </el-carousel>
     </div>
-    <div class="flex-1 flex">
-      <router-view></router-view>
+    <div class="flex-1 rightArea">
+      <div class="loginCard">
+        <div class="w-full h-100px c-#fff font-600 font-size-30px text-center">聊天室</div>
+
+        <el-form label-width="80px" label-position="left">
+          <el-form-item label="账号">
+            <el-input class="w-220px" />
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input class="w-220px" />
+          </el-form-item>
+        </el-form>
+
+        <div class="w-300px text-end">
+          <el-button size="small" type="primary" @click="config.login()">登录</el-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ChatDotRound, Setting, User } from '@element-plus/icons-vue'
-import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import { ChartIndex } from './config'
+import { reactive } from 'vue'
 
 const router = useRouter()
 
 const config = reactive({
-  activatedItem: 'message',
-  handleActivitedClick(name) {
-    this.activatedItem = name
-    router.push(name)
-  },
-  activedItemStyle(name) {
-    return name == this.activatedItem ? 'background-color:#88888850' : ''
+  async login() {
+    router.push('/home/chart-room/message')
   },
 })
 </script>
@@ -57,44 +48,39 @@ const config = reactive({
   width: 100vw;
   height: 100%;
 
-  background-color: green;
+  background-color: #303030;
 
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.leftArea {
-  // w-60px h-full bg-#22222f flex flex-wrap
-  width: 60px;
+.rightArea {
   height: 100%;
 
-  background-color: #22222f;
+  background-color: rgb(107, 109, 113);
+  display: flex;
+  justify-content: center;
+}
+
+.loginCard {
+  width: 500px;
+  height: min-content;
+
+  border-radius: 8px;
+  //   border: 2px black solid;
+
+  position: relative;
+  top: 20%;
 
   display: flex;
+  justify-content: center;
+  align-items: center;
+
   flex-wrap: wrap;
-  flex-direction: column;
-  gap: 10px;
 }
 
-.leftAreaAvatar {
-  cursor: pointer;
-  text-align: center;
-}
-
-.leftAreaItem {
-  box-sizing: border-box;
-  margin-left: 10px;
-  margin-right: 10px;
-  border-radius: 8px;
-
-  cursor: pointer;
-  text-align: center;
-
+::v-deep(.el-form-item__label) {
   color: #fff;
-  font-size: 25px;
-
-  transition: 0.1s;
-  &:hover {
-    background-color: #ffffff50 !important;
-  }
 }
 </style>
